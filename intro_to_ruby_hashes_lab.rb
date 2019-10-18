f5f35e8e9363de3d62f6ad54be4f2f31eb06b242
eunice kim <oonis86@gmail.com>
2019-10-18 03:57:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,11 +7,8 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  bash_hash = {
-    "nice" => "neat",
-    :railroads => {}
-  }
   bash_hash
+  bash_hash[:railroads] = {}
 end
 
 def monopoly_with_third_tier
