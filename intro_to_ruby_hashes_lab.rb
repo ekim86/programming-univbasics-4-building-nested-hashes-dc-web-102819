44f1efaf643f6885ee8237e0c39b963ec63c831d
eunice kim <oonis86@gmail.com>
2019-10-18 04:09:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,9 +22,9 @@ def monopoly_with_third_tier
 	    :pieces => 4,
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
-	      "2" => 2,
-	      "3" => 3,
-	      "4" => 4},
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 75,
+	      :four_pieces_owned => 100},
 	    :names => {
 	      "1" => 1,
 	      "2" => 2,
