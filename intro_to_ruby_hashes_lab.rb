0dd593154933291b4755404c2b94699188e53a86
eunice kim <oonis86@gmail.com>
2019-10-18 03:55:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,6 +8,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   bash_hash[:railroads] = {}
+  bash_hash
 end
 
 def monopoly_with_third_tier
